e1a3e838fa024e75d4403ae953ccf20eece0e92f
Chonlasit 666 <amazingmos@hotmail.com>
2020-12-31 16:30:35 +0700

    modified some index and view for show all username

--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_username_list = User.objects.order_by('-pub_date')[:5]
+    output =', '.join([q.username for q in latest_username_list])
+    return HttpResponse(output)
+
+
+def detail(request, user_id):
+    return HttpResponse("You're looking at username %s." % user_id)
